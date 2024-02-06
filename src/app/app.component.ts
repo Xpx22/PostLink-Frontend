bbe505753460a27d5b948ae3f542f8e65e8d146a
LuBeileBela <117722370+LuBeileBela@users.noreply.github.com>
2024-02-06 11:29:07 +0100

    Angular cdk and material 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,61 +2,74 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from './services/auth.service';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule } from '@angular/material/legacy-button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [MatSidenavModule, NgIf, NgFor, RouterLink, MatToolbarModule, MatLegacyButtonModule, MatIconModule, RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    NgIf,
+    NgFor,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet,
+  ],
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, OnDestroy {
   isSidenavOpen = false;
   brand = 'PostLink';
-  signinText = "Sign In";
-  signOutText = "Sign out";
-  userChoices: { text: string, nav: string}[] = [
-    {text: "Account", nav: "/users/account"},
-    {text: "Dashboard", nav: "/users/dashboard"},
-    {text: "Bookmarks", nav: "/users/bookmarks"},
+  signinText = 'Sign In';
+  signOutText = 'Sign out';
+  userChoices: { text: string; nav: string }[] = [
+    { text: 'Account', nav: '/users/account' },
+    { text: 'Dashboard', nav: '/users/dashboard' },
+    { text: 'Bookmarks', nav: '/users/bookmarks' },
   ];
-  employerChoices: { text: string, nav: string}[] = [
-    {text: "Account", nav: "employers/account"},
-    {text: "Dashboard", nav: "/employers/dashboard"},
-    {text: "Post a job", nav: "/employers/create"},
-  ];;
-  private userAuthSubscription = new Subscription;
-  private employerAuthSubscription = new Subscription;
+  employerChoices: { text: string; nav: string }[] = [
+    { text: 'Account', nav: 'employers/account' },
+    { text: 'Dashboard', nav: '/employers/dashboard' },
+    { text: 'Post a job', nav: '/employers/create' },
+  ];
+  private userAuthSubscription = new Subscription();
+  private employerAuthSubscription = new Subscription();
   userIsAuthenticated = false;
   employerIsAuthenticated = false;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.userAuthSubscription = this.authService.getUserAuthStateListener().subscribe((isAuthenticated)=>{
-      this.userIsAuthenticated = isAuthenticated;
-    });
-    this.employerAuthSubscription = this.authService.getEmployerAuthStateListener().subscribe((isAuthenticated)=>{
-      this.employerIsAuthenticated = isAuthenticated;
-    });
+    this.userAuthSubscription = this.authService
+      .getUserAuthStateListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+    this.employerAuthSubscription = this.authService
+      .getEmployerAuthStateListener()
+      .subscribe((isAuthenticated) => {
+        this.employerIsAuthenticated = isAuthenticated;
+      });
     this.authService.refreshToken();
   }
 
-  toggleSidenav(){
+  toggleSidenav() {
     this.isSidenavOpen = !this.isSidenavOpen;
   }
 
-  onSignOut(){
+  onSignOut() {
     this.authService.signOut();
     this.isSidenavOpen = false;
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.userAuthSubscription.unsubscribe();
     this.employerAuthSubscription.unsubscribe();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
-import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/material/legacy-paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
-import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app/app-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -26,19 +11,48 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { ErrorInterceptor } from './app/interceptors/error-interceptor';
 import { AuthInterceptor } from './app/interceptors/auth-interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, ReactiveFormsModule, FormsModule, AppRoutingModule, MatToolbarModule, MatCardModule, MatButtonModule, MatInputModule, MatMenuModule, MatRadioModule, MatCheckboxModule, MatSlideToggleModule, MatSidenavModule, MatIconModule, MatPaginatorModule, MatProgressSpinnerModule, MatChipsModule, MatDialogModule, MatSnackBarModule, MatListModule),
-        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      ReactiveFormsModule,
+      FormsModule,
+      AppRoutingModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatInputModule,
+      MatMenuModule,
+      MatRadioModule,
+      MatSlideToggleModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatPaginatorModule,
+      MatProgressSpinnerModule,
+      MatDialogModule,
+      MatSnackBarModule,
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.error(err));
